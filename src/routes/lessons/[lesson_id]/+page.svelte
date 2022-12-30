1ceb75d84ef245c85a475191d7110923ac678d32
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-29 20:46:54 -0600

    remove logging

--- src/routes/lessons/[lesson_id]/+page.svelte
+++ src/routes/lessons/[lesson_id]/+page.svelte
@@ -2,8 +2,6 @@
   import Lesson from "$lib/Lesson.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
-
-  console.log(data)
 </script>
 
 <Lesson lesson={data.lesson} />
