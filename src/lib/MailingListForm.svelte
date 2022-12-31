59e29081ab673ffbbc0185821beb4f4784ee84df
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-31 17:20:46 -0600

    Honeypot for form

--- src/lib/MailingListForm.svelte
+++ src/lib/MailingListForm.svelte
@@ -2,10 +2,18 @@
 <!-- Then use it in multiple places -->
 
 <script lang="ts">
+    import { goto } from "$app/navigation";
+
+
   let name = "";
   let email = "";
+  let hidden = "";
 
   const submit = async () => {
+    if(hidden) {
+      alert("Thanks for trying to sign up! But you've been marked as a robot.  Try again if you're human, and maybe don't fill out the hidden honeypot field this time.")
+      return;
+    }
     const response = await fetch("/api/mailing-list", {
       method: "POST",
       headers: {
@@ -24,12 +32,12 @@
   };  
 </script>
 
-<h1>Sign up for Mailing List</h1>
 <form on:submit|preventDefault={submit}>
   <label for="email">Email</label>
   <input type="email" name="email" id="email" bind:value={email} />
   <label for="name">Name</label>
   <input type="text" name="name" id="name" bind:value={name} />
+  <input type="text" name="honeypot" id="honeypot" bind:value={hidden} />
   <button type="submit">Submit</button>
 </form>
 
@@ -37,10 +45,20 @@
   form {
     display: flex;
     flex-direction: column;
+    width: 600px;
+  }
+  button {
+    cursor: pointer;
   }
   input, button {
     padding: 8px;
     margin-bottom: 16px;
     margin-top: 4px;
+  }
+  #honeypot {
+    visibility: hidden;
+    height: 2px;
+    margin: 0;
+    padding: 0;
   }
 </style>
