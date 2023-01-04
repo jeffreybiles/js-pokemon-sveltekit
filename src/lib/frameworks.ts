0f0381f2c8f0aa0ad98b5153da84a3fa898eea36
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-01-04 09:23:00 -0600

    Make front-page somewhat presentable

--- src/lib/frameworks.ts
+++ src/lib/frameworks.ts
@@ -1,6 +1,14 @@
 export type FrameworkName = 'SvelteKit 1.0' | 'Nuxt 3' | 'Next 13' | 'Remix 1.10'
 
 export const frameworks: {[key in FrameworkName]: {icon: string, color: string}} = {
+  'SvelteKit 1.0': {
+    icon: 'vscode-icons:file-type-svelte',
+    color: '#FF3E00'
+  },
+  'Nuxt 3': {
+    icon: 'vscode-icons:file-type-nuxt',
+    color: '#38C032'
+  },
   'Next 13': {
     icon: 'logos:nextjs-icon',
     color: '#000000'
@@ -9,14 +17,7 @@ export const frameworks: {[key in FrameworkName]: {icon: string, color: string}}
     icon: 'logos:remix-icon',
     color: '#FF4D5B'
   },
-  'Nuxt 3': {
-    icon: 'vscode-icons:file-type-nuxt',
-    color: '#38C032'
-  },
-  'SvelteKit 1.0': {
-    icon: 'vscode-icons:file-type-svelte',
-    color: '#FF3E00'
-  }
+
 }
 
 export const frameworkArray = Object.keys(frameworks).map((name: FrameworkName) => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
   import MailingListForm from "$lib/MailingListForm.svelte";
   import Iconify from "@iconify/svelte";
-  import { frameworkArray } from "$lib/frameworks";
+  import { frameworkArray, type FrameworkName } from "$lib/frameworks";
   import { goto } from "$app/navigation";
+  import type { PageData } from "./$types";
 
   const startLearning = (framework: string) => {
     localStorage.setItem('selectedFramework', framework);
     goto('/lessons/101');
   }
+
+  export let data: PageData;
+
+  const numLessons = (framework: FrameworkName) => {
+    return data.lessons.filter((lesson) => {
+      return lesson.implementations.find(impl => impl.framework === framework)
+    }).length;
+  };
 </script>
 <h1>Welcome to the Pokedex Tutorial</h1>
-<h2>Learn and compare Next 13, Remix 1.10, Nuxt 3, and SvelteKit 1.0</h2>
-<p class="status-report">Status: season 1 (22 lessons) is planned, and the SvelteKit version is fully built out.</p>
+<p>This video tutorial series will build the same app in 4 different Javascript meta-frameworks - Next 13, Remix 1.10, Nuxt 3, and SvelteKit 1.0</p>
+<p>Source code and demo site are available for each lesson.</p>
+<p class="status-report">Status: season 1 (22 lessons per framework) is planned, and the SvelteKit version is fully built out.</p>
+<p class="status-report">I have begun to film and release the SvelteKit tutorial series.  Other series coming soon.</p>
 
 <div class="framework-buttons">
   {#each frameworkArray as framework (framework.name)}
@@ -19,8 +30,9 @@
       class="framework-button"
       on:click={() => startLearning(framework.name)}
       style="background-color: {framework.color};"
+      class:disabled={numLessons(framework.name) === 0}
     >
-      Start Learning {framework.name}
+      Learn {framework.name}
       <Iconify icon={framework.icon} />
     </button>
   {/each}
@@ -58,18 +70,25 @@
     cursor: pointer;
     gap: 1rem;
   }
+  .framework-button.disabled {
+    opacity: 0.3;
+    cursor: not-allowed;
+  }
   .framework-button:hover {
     opacity: 0.8;
   }
   .status-report {
     font-style: italic;
     font-weight: 200;
-    font-size: 1.2rem;
-    margin: 1rem 0;
+    font-size: 1rem;
+    margin: 0.5rem 0;
   }
   .iframe-container {
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
     border-radius: 5px;
     margin: 2rem 0;
+  }
+  p, h1 {
+    color: #333;
   }
 </style>
