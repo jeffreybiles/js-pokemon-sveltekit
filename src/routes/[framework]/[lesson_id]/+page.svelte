b1e0bf8a91d6f43aa8510d417d6bd98254b636fe
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-01-05 13:36:42 -0600

    More links on the video page

--- src/routes/[framework]/[lesson_id]/+page.svelte
+++ src/routes/[framework]/[lesson_id]/+page.svelte
@@ -4,6 +4,8 @@
   import VideoPlayer from "$lib/VideoPlayer.svelte";
   import Navigation from "$lib/Navigation.svelte";
   import type { PageData } from "./$types";
+    import Icon from "@iconify/svelte";
+    import type { Implementation } from "src/routes/api/lessons/lessons";
 
   export let data: PageData;
 
@@ -11,11 +13,23 @@
   $: lesson_id = $page.params.lesson_id;
   $: framework = frameworkArray.find((f) => f.slug === frameworkSlug);
   $: lesson = data.lesson
-  $: implementation = lesson?.implementations.find((i) => i.framework === framework?.name);
+  $: implementation = lesson?.implementations.find((i: Implementation) => i.framework === framework?.name);
 </script>
 
 {#if lesson}
-  <h1>{lesson?.name}</h1>
+  <div class="flex-between">
+    <h1>{lesson?.name}</h1>
+    {#if implementation?.lengthInSeconds}
+      <h1 class="flex-between">
+        <a href="https://www.youtube.com/watch?v={implementation?.youtubeId}" target="_blank" rel="noreferrer" class="flex-between">
+          <Icon icon="mdi:youtube" style="margin-right: 10px;" />
+        </a>
+        <a href={implementation?.githubUrl} target="_blank" rel="noreferrer" class="flex-between">
+          <Icon icon="mdi:github" />
+        </a>
+      </h1>
+    {/if}
+  </div>
 
   {#if implementation?.youtubeId}
     <VideoPlayer youtubeId={implementation.youtubeId} />
@@ -36,4 +50,14 @@
 {/if}
 
 <!-- TODO - links to other implementations -->
-<!-- TODO - next/previous/index -->
+
+<style>
+  .flex-between {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  a {
+    color: black;
+  }
+</style>
